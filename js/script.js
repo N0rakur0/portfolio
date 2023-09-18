7666f93daad94d8f655d5f7edb92be8d37868290
N0rakur0 <137528396+N0rakur0@users.noreply.github.com>
2023-09-19 04:21:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// ロード画面
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loaderWrapper = document.querySelector(".loader-wrapper");
+
+    // 5秒後にローディング画面を非表示にする（フェードアウト付き）
+    setTimeout(function() {
+        loaderWrapper.classList.add("hidden"); // フェードアウトクラスを追加
+    }, 5000); // 5秒（5000ミリ秒）の遅延時間を設定
+});
+
+
+// 横スクロール
 const wrapper = document.querySelector('#scroll');
 if (wrapper) {
     // gsap.registerPlugin(ScrollTrigger); // npm/yarnの際に必要
